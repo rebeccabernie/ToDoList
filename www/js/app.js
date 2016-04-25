315a2cbab9e1e21fe86f335d0850fead32a9693e
Rebecca Kane <rebecca.kane@outlook.com>
2016-04-25 17:32:03 +0100

    Nearly There
    App working, yet to do comments/tidy up code.

--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,6 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+angular
+  
+.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -54,19 +33,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'UpcomingCtrl'
         }
       }
-    })
-
-  .state('tab.complete', {
-    url: '/complete',
-    views: {
-      'tab-complete': {
-        templateUrl: 'templates/tab-complete.html',
-        controller: 'CompleteCtrl'
-      }
-    }
-  });
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/add');
+  $urlRouterProvider.otherwise('/tab/upcoming');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('AddCtrl', function($scope) {})
+.controller('AddCtrl', function($scope, Task) {
+    
+    $scope.data = {
+        title: "", details: ""
+    };
+    
+    $scope.addTask = function(){
+      
+        Task.addTask($scope.data.title, $scope.data.details);
+            $scope.data.title = "";
+            $scope.data.details = "";   
+    };
+})
 
-.controller('UpcomingCtrl', function($scope, Chats) {})
 
-.controller('CompletedCtrl', function($scope) {});
+.controller('UpcomingCtrl', function($scope, Task) {
+    $scope.data = "";
+         $scope.complete = function(i){
+            Task.complete(i);
+        };
+    
+    $scope.tasks = Task.data.tasks;
+    $scope.show;
+})
